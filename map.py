8d761709337aad6f18c8c041e0f1e14368938559
Teemu Kakko <sormiteippi@windowslive.com>
2023-03-12 18:53:23 +0200

    Add layer control

--- map.py
+++ map.py
@@ -7,7 +7,7 @@ latitudes = []
 longitudes = []
 
 # Open the GEDCOM file
-with open("family_tree.ged", "r", encoding="utf-8") as f:
+with open('family_tree.ged', 'r', encoding='utf-8') as f:
 
     # Initialize some variables
     current_place = None
@@ -20,21 +20,21 @@ with open("family_tree.ged", "r", encoding="utf-8") as f:
         # Strip any leading or trailing whitespace from the line
         line = line.strip()
         
-        # Check if the line starts with "2 PLAC"
-        if line.startswith("2 PLAC"):
+        # Check if the line starts with `2 PLAC`
+        if line.startswith('2 PLAC'):
                 
             # Update the current place
             current_place = line[7:]
             latitude = None
             longitude = None
         
-        # Check if the line starts with "4 LATI"
-        elif line.startswith("4 LATI"):
-            latitude = line.split(" ")[-1][1:]
+        # Check if the line starts with `4 LATI`
+        elif line.startswith('4 LATI'):
+            latitude = line.split(' ')[-1][1:]
             
-        # Check if the line starts with "4 LONG"
-        elif line.startswith("4 LONG"):
-            longitude = line.split(" ")[-1][1:]
+        # Check if the line starts with `4 LONG`
+        elif line.startswith('4 LONG'):
+            longitude = line.split(' ')[-1][1:]
     
         # If we have a current place, latitude, and longitude, add them to the lists
         if current_place and latitude and longitude:
@@ -49,7 +49,7 @@ with open("family_tree.ged", "r", encoding="utf-8") as f:
 
 # Check if we found any locations in the GEDCOM file
 if len(placenames) == 0:
-    print("No locations found in GEDCOM file.")
+    print('No locations found in GEDCOM file.')
 else:
     # Create a map centered on the first location
     m = folium.Map(location=[float(latitudes[0]), float(longitudes[0])], zoom_start=7)
@@ -57,15 +57,24 @@ else:
     # Create a list of coordinate pairs
     coordinates = list(zip(latitudes, longitudes))
 
-    # Add markers for each location to the map
-    for name, lat, lon in zip(placenames, latitudes, longitudes):
-        folium.Marker([float(lat), float(lon)], popup=name).add_to(m)
-
     # Create a heatmap layer from the coordinates
-    heat_layer = HeatMap(coordinates)
+    heat_layer = HeatMap(coordinates, name='Heatmap')
 
     # Add the heatmap layer to the map
     heat_layer.add_to(m)
 
+    # Create a layer group for the markers
+    marker_group = folium.FeatureGroup(name='Markers', show=False)
+
+    # Add markers for each location to the map
+    for name, lat, lon in zip(placenames, latitudes, longitudes):
+        folium.Marker([float(lat), float(lon)], popup=name).add_to(marker_group)
+
+    # Add the marker group to the map
+    marker_group.add_to(m)
+
+    # Add a layer control to the map to toggle the markers
+    folium.LayerControl(collapsed=False).add_to(m)
+
     # Display the map
-    m.save("build/map.html")
+    m.save('build/map.html')
